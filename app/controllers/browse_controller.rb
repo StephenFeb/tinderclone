693a0865c1d36ff646f6be99518bc8caae048522
StephenFeb <spinshon@hotmail.com>
2020-08-25 00:12:29 +0100

    adding a browse function

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,6 +1,7 @@
 class BrowseController < ApplicationController
 
 def browse
+    @users = Account.where.not(id: current_account.id)
 end
 
 def approve
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Account.create([
+    {first_name: "Sarah", last_name: "Jones", username: "SJanes", email: "hello@test.com", password: "Password1"},
+    {first_name: "Rachel", last_name: "Smith", username: "Rach", email: "hello1@test.com", password: "Password2"},
+    {first_name: "Lisa", last_name: "Blogs", username: "Lisa1", email: "hello2@test.com", password: "Password3"},
+    {first_name: "Monica", last_name: "Geller", username: "Mon", email: "hello3@test.com", password: "Password4"},
+    {first_name: "Penny", last_name: "Davies", username: "Pen", email: "hello4@test.com", password: "Password5"},
+
+
+])
